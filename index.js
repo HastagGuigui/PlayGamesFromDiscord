a63ca59e6ee43be5f9dadc9e471390c3dd37bbce
#Guigui <47950669+HastagGuigui@users.noreply.github.com>
2020-11-15 21:04:11 +0100

    Adding hold feature
    To use it, do <prefix>hold <key> [delay] (by default, 1 second)
    
    Aliases: <prefix>h

--- index.js
+++ index.js
@@ -13,16 +13,18 @@ client.on('message', message => {
     let args = message.content.slice(prefix.length).trim().split(' ');
     let cmd = args.shift().toLowerCase();
     switch (cmd) {
+        case "press":
         case "p":
             var plaque_de_pression = args[0]
             ks.setOption('globalDelayPressMillisec', config.globalDelay);
             ks.setOption('startDelayMillisec', config.pressDelay);
-            ks.setOption('startDelayMillisec', config.delayBetweenPresses);
+            ks.setOption('globalDelayBetweenMillisec', config.delayBetweenPresses);
+            var numberOfPresses = args[1] || "1"
             if (args[0]){
-                console.log("[" + args[0].toUpperCase() + " x " + args[1] + "] par " + message.author.tag)
+                console.log("[" + args[0].toUpperCase() + " x " + numberOfPresses + "] par " + message.author.tag)
             }
             if (args[1]) {
-                if (args[1] >= 100) args[1] = 100
+                if (parseInt(args[1]) >= 100) args[1] = 100
                 let i;
                 for (i = 0; i < parseInt(args[1]); i++) {
                     sendKeyFromDiscord(plaque_de_pression, i, parseInt(args[1]))
@@ -30,89 +32,100 @@ client.on('message', message => {
             }else{
                 sendKeyFromDiscord(plaque_de_pression, 1, 1)
             }
-            function sendKeyFromDiscord(x, i, limit) {
-                switch (x) {
-                    case "haut":
-                        ks.sendKey(config.keymapcontrols.DPadUp);
-                        break
-                    case "up":
-                        ks.sendKey(config.keymapcontrols.JoystickUp);
-                        break;
-    
-                    case "gauche":
-                        ks.sendKey(config.keymapcontrols.DPadLeft);
-                        break
-                    case "left":
-                        ks.sendKey(config.keymapcontrols.JoystickLeft);
-                        break;
-    
-                    case "droite":
-                        ks.sendKey(config.keymapcontrols.DPadRight);
-                        break
-                    case "right":
-                        ks.sendKey(config.keymapcontrols.JoystickRight);
-                        break;
-                    
-                    case "bas":
-                        ks.sendKey(config.keymapcontrols.DPadDown);
-                        break
-                    case "down":
-                        ks.sendKey(config.keymapcontrols.JoystickDown);
-                        break;
-    
-                        
-                    case "a":
-                        ks.sendKey(config.keymapcontrols.A);
-                        break
-                    case "b":
-                        ks.sendKey(config.keymapcontrols.B);
-                        break;
-                    case "x":
-                        ks.sendKey(config.keymapcontrols.X);
-                        break
-                    case "y":
-                        ks.sendKey(config.keymapcontrols.Y);
-                        break;
-    
-                    
-                    case "l":
-                        ks.sendKey(config.keymapcontrols.L);
-                        break
-                    case "r":
-                        ks.sendKey(config.keymapcontrols.R);
-                        break;
-                    case "zl":
-                        ks.sendKey(config.keymapcontrols.ZL);
-                        break
-                    case "zr":
-                        ks.sendKey(config.keymapcontrols.ZR);
-                        break;
-    
-                    
-                    case "start":
-                        ks.sendKey(config.keymapcontrols.Start);
-                        break
-                    case "select":
-                        ks.sendKey(config.keymapcontrols.Select);
-                        break;
-    
-    
-                    default:
-                        var helpembed = new Discord.MessageEmbed()
-                            .setTitle("Menu D'aide")
-                            .setDescription("Syntaxe: "+ config.prefix +".p [touche] <nombre de fois>")
-                            .addField("Touches valides:", "Joystick Gauche: \n -up \n -down \n -left \n -right \n \n Pad directionnel: \n -haut \n -bas \n -gauche \n -droite \n \n Boutons de base: \n -a \n -b \n -x \n -y \n -start \n -select \n -l \n -r \n -zl \n -zr")
-                            .setFooter("[obligatoire] <optionel>")
-                        message.channel.send(helpembed);
-                        i = limit;
-                        break;
-            }
-            }
             break;
+        
+        case "hold":
+        case "h":
+            var plaque_de_pression = args[0]
+            ks.setOption('globalDelayPressMillisec', Math.min(parseFloat(args[1]) * 1000, 10000));
+            ks.setOption('startDelayMillisec', config.pressDelay);
+            ks.setOption('globalDelayBetweenMillisec', config.delayBetweenPresses);
+            console.log("[" + args[0].toUpperCase() + " pendant " + args[1] + " seconde(s)] par " + message.author.tag)
+            sendKeyFromDiscord(plaque_de_pression, 1, 1)
+        break
     
         default:
+            var helpembed = new Discord.MessageEmbed()
+                .setTitle("Menu D'aide")
+                .setDescription("Syntaxe: \n"+ config.prefix +"p [touche] <nombre de fois> \n"+ config.prefix + "h [touche]")
+                .addField("Touches valides:", "-gauche \n-droite \n-haut \n-bas \n\n-a\n-b\n-y\n-x \n-start\n-select")
+                .setFooter("[obligatoire] <optionel>")
+            message.channel.send(helpembed);
             break;
     }
 })
 
 client.login(config.token)
+
+function sendKeyFromDiscord(x, i, limit) {
+    switch (x) {
+        case "haut":
+            ks.sendKey(config.keymapcontrols.DPadUp);
+            break
+        case "up":
+            ks.sendKey(config.keymapcontrols.JoystickUp);
+            break;
+
+        case "gauche":
+            ks.sendKey(config.keymapcontrols.DPadLeft);
+            break
+        case "left":
+            ks.sendKey(config.keymapcontrols.JoystickLeft);
+            break;
+
+        case "droite":
+            ks.sendKey(config.keymapcontrols.DPadRight);
+            break
+        case "right":
+            ks.sendKey(config.keymapcontrols.JoystickRight);
+            break;
+        
+        case "bas":
+            ks.sendKey(config.keymapcontrols.DPadDown);
+            break
+        case "down":
+            ks.sendKey(config.keymapcontrols.JoystickDown);
+            break;
+
+            
+        case "a":
+            ks.sendKey(config.keymapcontrols.A);
+            break
+        case "b":
+            ks.sendKey(config.keymapcontrols.B);
+            break;
+        case "x":
+            ks.sendKey(config.keymapcontrols.X);
+            break
+        case "y":
+            ks.sendKey(config.keymapcontrols.Y);
+            break;
+
+        
+        case "l":
+            ks.sendKey(config.keymapcontrols.L);
+            break
+        case "r":
+            ks.sendKey(config.keymapcontrols.R);
+            break;
+        case "zl":
+            ks.sendKey(config.keymapcontrols.ZL);
+            break
+        case "zr":
+            ks.sendKey(config.keymapcontrols.ZR);
+            break;
+
+        
+        case "start":
+            ks.sendKey(config.keymapcontrols.Start);
+            break
+        case "select":
+            ks.sendKey(config.keymapcontrols.Select);
+            break;
+
+
+        default:
+            i = limit;
+            break;
+}
+}
